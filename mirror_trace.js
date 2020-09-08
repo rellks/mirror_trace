e49b9b987e66b171c10fc03e798152bec591a568
Richard <rellks@ucsd.edu>
2020-09-07 18:55:05 -0700

    update star start point

--- mirror_trace.js
+++ mirror_trace.js
@@ -18,8 +18,8 @@
 var materials = {
 		'mirror' : [false, true, true, true, true, true, true, true],
 		'file_names' : ["https://raw.githubusercontent.com/rcalinjageman/mirror_trace/master/sample.png", "https://rellks.github.io/mirror_trace/Star.png", "https://raw.githubusercontent.com/rcalinjageman/mirror_trace/master/trialh2.png", "https://raw.githubusercontent.com/rcalinjageman/mirror_trace/master/trialh3.png", "https://rellks.github.io/mirror_trace/Star.png", "https://raw.githubusercontent.com/rcalinjageman/mirror_trace/master/trial2.png", "https://raw.githubusercontent.com/rcalinjageman/mirror_trace/master/trial3.png", "https://raw.githubusercontent.com/rcalinjageman/mirror_trace/master/sample.png"],
-		'xstarts' : [47,	27,		40,		280,		150, 40, 280, 33],
-		'ystarts' : [256,	275,	45,		276,		150, 45, 276, 250],
+		'xstarts' : [47,	27,		40,		280,		200, 40, 280, 33],
+		'ystarts' : [256,	275,	45,		276,		75, 45, 276, 250],
 		'xends' :   [344,	370,	368,	33,			370, 368, 33, 47],
 		'yends' :    [260,	28,		267,	250,		28,267,250, 256]
 	}
