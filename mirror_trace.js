83e6f5dbc6cfef660ae77d2698612862045a7925
rellks <rellks@ucsd.edu>
2021-02-02 10:40:04 -0800

    move start, mid, and end points

--- mirror_trace.js
+++ mirror_trace.js
@@ -2,11 +2,11 @@ const materials = {
 		'mirror' : [true, true, true, true],
 		'file_names' : [ "https://rellks.github.io/mirror_trace/Star-easy.png", "https://rellks.github.io/mirror_trace/Star-medium.png", "https://rellks.github.io/mirror_trace/Star-hard.png", "https://rellks.github.io/mirror_trace/Star.png"],
 		'xstarts' : [200, 200, 200, 200],
-		'ystarts' : [95, 100, 100, 100],		
+		'ystarts' : [80, 100, 100, 100],		
 		'xmids' : [200, 200, 200, 200],
-		'ymids' : [240, 240, 240, 240],
+		'ymids' : [260, 240, 240, 240],
 		'xends' :  [200, 200, 200, 200],
-		'yends' :    [100, 100, 100, 100]
+		'yends' :    [80, 100, 100, 100]
 	}
 	
 	// this script can save screenshots of completed trials.  
